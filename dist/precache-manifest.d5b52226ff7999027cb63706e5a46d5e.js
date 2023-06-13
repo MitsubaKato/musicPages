f90e6b18d66027c26e1390af67f22750e2160130
MitsubaKato <alexreals78@gmail.com>
2023-06-13 16:09:22 +0300

    add dist

---
+++ dist/precache-manifest.d5b52226ff7999027cb63706e5a46d5e.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d8c7bce9e47d0946e4a51c7b3e2e80",
+    "url": "/MusicApp/assets/img/header.png"
+  },
+  {
+    "revision": "48025182349af4abfe3e272a7e128c4c",
+    "url": "/MusicApp/assets/img/introduction-music.png"
+  },
+  {
+    "revision": "90840bfca190b45f7cd04ed9d8c2cd54",
+    "url": "/MusicApp/assets/img/introduction-users.png"
+  },
+  {
+    "revision": "eba639c1b0a9287dd3a1904b9ae9831f",
+    "url": "/MusicApp/assets/img/profile-pic.jpg"
+  },
+  {
+    "revision": "d899ba7d2cac3f11c4377a700d68d22e",
+    "url": "/MusicApp/assets/img/song-header.png"
+  },
+  {
+    "revision": "e7b9aeb02c86a922404d13073d21836a",
+    "url": "/MusicApp/assets/img/user-header.png"
+  },
+  {
+    "revision": "1506223cb9d5aa18471f",
+    "url": "/MusicApp/css/app.dc3efdf7.css"
+  },
+  {
+    "revision": "2a28535aff150f70fe2a",
+    "url": "/MusicApp/css/chunk-2a3ed3a8.e6b1c83b.css"
+  },
+  {
+    "revision": "beedf97c3020c111dab2",
+    "url": "/MusicApp/css/chunk-59bb55ac.820e535e.css"
+  },
+  {
+    "revision": "b40c624dfcd8dd2a173b",
+    "url": "/MusicApp/css/chunk-69af673b.ff990faa.css"
+  },
+  {
+    "revision": "70f7adf038a1f2f1710b",
+    "url": "/MusicApp/css/chunk-7617f008.f3b4539d.css"
+  },
+  {
+    "revision": "d0e34d7237053053d74d",
+    "url": "/MusicApp/css/chunk-vendors.35ea3ac6.css"
+  },
+  {
+    "revision": "35f4db720815c07163a0",
+    "url": "/MusicApp/css/groupedChunk.9c9dc34c.css"
+  },
+  {
+    "revision": "32a16305887e3e14b1c507ae67f45af8",
+    "url": "/MusicApp/index.html"
+  },
+  {
+    "revision": "1506223cb9d5aa18471f",
+    "url": "/MusicApp/js/app.3ac6a060.js"
+  },
+  {
+    "revision": "2a28535aff150f70fe2a",
+    "url": "/MusicApp/js/chunk-2a3ed3a8.aeffa161.js"
+  },
+  {
+    "revision": "beedf97c3020c111dab2",
+    "url": "/MusicApp/js/chunk-59bb55ac.83f99105.js"
+  },
+  {
+    "revision": "b40c624dfcd8dd2a173b",
+    "url": "/MusicApp/js/chunk-69af673b.efbdbef9.js"
+  },
+  {
+    "revision": "70f7adf038a1f2f1710b",
+    "url": "/MusicApp/js/chunk-7617f008.8491c8ac.js"
+  },
+  {
+    "revision": "d0e34d7237053053d74d",
+    "url": "/MusicApp/js/chunk-vendors.c9eb1a20.js"
+  },
+  {
+    "revision": "35f4db720815c07163a0",
+    "url": "/MusicApp/js/groupedChunk.4f774812.js"
+  },
+  {
+    "revision": "f7216665154ef9db8b6fb3462c035e27",
+    "url": "/MusicApp/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/MusicApp/robots.txt"
+  }
+]);
